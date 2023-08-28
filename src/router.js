4f70027d7bcb5de1d30f937e2cb41b4dbc99a97b
Assoidi <assoidi@yahoo.fr>
2023-08-28 15:19:39 +0200

    feat(rooting): initialisation des routes

--- src/router.js
+++ src/router.js
@@ -8,5 +8,14 @@ export const routes = [
   { path: '/', component: <HomePage /> },
   { path: '/login', component: <LoginPage /> },
   { path: '/users', component: <UsersPage /> },
-  { path: '/register', component: <SignUpPage /> }
+  { path: '/users/:id', component: <UsersPage /> },
+  { path: '/customers', component: <UsersPage /> },
+  { path: '/customers/:id', component: <UsersPage /> },
+  { path: '/register', component: <SignUpPage /> },
+  { path: '/restaurants', component: <UsersPage /> },
+  { path: '/restaurants/:id', component: <UsersPage /> },
+  { path: '/menus', component: <UsersPage /> },
+  { path: '/menus/:id', component: <UsersPage /> },
+  { path: '/delivery-drivers', component: <UsersPage /> },
+  { path: '/delivery-drivers/:id', component: <UsersPage /> },
 ]
